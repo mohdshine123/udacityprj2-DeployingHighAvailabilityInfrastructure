68e1dd7c15b3b8c80b9f2b32ae27bd023eac91f9
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2023-09-14 19:12:24 +0530

    Update main.tf

--- solution-deploying-terraform-assets/exercises/starternew/main.tf
+++ solution-deploying-terraform-assets/exercises/starternew/main.tf
@@ -1,5 +1,5 @@
 module "ec2_instance" {
-  source         = "./ec2_instance"
+  source         = "./modules/EC2"
   instance_type  = "t2.micro"
   instance_count = 4
   vpc_id         = "vpc-077baef119aed7908"
