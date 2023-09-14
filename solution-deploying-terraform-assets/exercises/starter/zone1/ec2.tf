abe48858798a6788ef938b0af1f4f52db668c969
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2023-09-14 17:56:12 +0530

    Update ec2.tf

--- solution-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
+++ solution-deploying-terraform-assets/exercises/starter/zone1/ec2.tf
@@ -1,10 +1,10 @@
   module "project_ec2" {
    source             = 
-   instance_count     = 
+   instance_count     = 1
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
    aws_ami            = "ami-0b9064170e32bde34"
-   private_subnet_ids = module.vpc.private_subnet_ids
-   public_subnet_ids  = 
-   vpc_id             = 
+   #private_subnet_ids = module.vpc.private_subnet_ids
+   public_subnet_ids  =  "subnet-089f49c4d44449059"
+   vpc_id             =  "vpc-077baef119aed7908"
  }
