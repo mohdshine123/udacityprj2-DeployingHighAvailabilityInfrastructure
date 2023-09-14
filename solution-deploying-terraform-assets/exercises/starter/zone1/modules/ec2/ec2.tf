76360e08b61048ca0b1112146c96dadbb45037d2
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2023-09-14 18:14:59 +0530

    Update ec2.tf

--- solution-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
+++ solution-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "ubuntu" {
-  ami           = 
-  count = 
-  instance_type = 
+  ami           = "ami-0b9064170e32bde34"
+  count = 1
+  instance_type = "t2.micro"
   subnet_id = var.public_subnet_ids[0]
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
-    Name = ""
+    Name = "webserver"
   }
 }
 
