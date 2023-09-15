cd134f74d764a93fb57f966ce7dd59927e2b1294
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2023-09-15 17:22:29 +0530

    Add files via upload

---
+++ solution-deploying-terraform-assets/exercises/Zone1deployment/RDSDeployment/rds-p/var.tf
@@ -0,0 +1,11 @@
+variable "private_subnet_ids" {
+  description = "Subnets for RDS Instances"
+  type        = "list"
+  default = [["subnet-09cb7ebf0a6d90c21", "subnet-0bf719c0c9206918c"]]
+}
+
+variable "vpc_id" {
+  description = "VPC for RDS instance"
+  type        = string
+  default     = "10.0.0.0/16"
+}
