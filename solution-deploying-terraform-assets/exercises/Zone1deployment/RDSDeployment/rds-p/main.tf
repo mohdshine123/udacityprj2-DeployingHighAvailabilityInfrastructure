ea79c6cd496b7944b5f8446bf66a97e84a7441de
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2023-09-15 21:16:09 +0530

    Update main.tf

--- solution-deploying-terraform-assets/exercises/Zone1deployment/RDSDeployment/rds-p/main.tf
+++ solution-deploying-terraform-assets/exercises/Zone1deployment/RDSDeployment/rds-p/main.tf
@@ -1,77 +1,74 @@
-provider "aws" {
-  region = "us-east-2"
-}
+data "aws_availability_zones" "available" {}
 
-resource "aws_rds_cluster_parameter_group" "cluster_pg" {
-  name   = "udacity-pg-p"
-  family = "aurora5.6"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
 
-  parameter {
-    name  = "binlog_format"    
-    value = "MIXED"
-    apply_method = "pending-reboot"
-  }
-
-  parameter {
-    name = "log_bin_trust_function_creators"
-    value = 1
-    apply_method = "pending-reboot"
-  }
+  name                 = "education"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 resource "aws_db_subnet_group" "udacity_db_subnet_group" {
   name       = "udacity_db_subnet_group"
-  subnet_ids = var.private_subnet_ids[0]
+  subnet_ids = module.vpc.public_subnets
 
-}
-resource "aws_rds_cluster" "udacity_cluster" {
-  cluster_identifier       = "udacity-db-cluster"
-  availability_zones       = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_pg.name
-  database_name            = "udacityc2"
-  master_username          = "udacity"
-  master_password          = "MyUdacityPassword"
-  vpc_security_group_ids   = [aws_security_group.db_sg_1.id]
-  db_subnet_group_name     = aws_db_subnet_group.udacity_db_subnet_group.name
-  engine_mode              = "provisioned"
-  engine_version           = "5.6.mysql_aurora.1.19.1" 
-  skip_final_snapshot      = true
-  storage_encrypted        = false
-  backup_retention_period  = 5
-  depends_on = [aws_rds_cluster_parameter_group.cluster_pg]
+  tags = {
+    Name = "udacity_db_subnet_group"
+  }
 }
 
-output "db_cluster_arn" {
-  value = aws_rds_cluster.udacity_cluster.arn
-}
-
-output "db_instance_arn" {
-  value = aws_rds_cluster_instance.udacity_instance[1].arn
-}
-
-resource "aws_rds_cluster_instance" "udacity_instance" {
-  count                = 2
-  identifier           = "udacity-db-instance-${count.index}"
-  cluster_identifier   = aws_rds_cluster.udacity_cluster.id
-  instance_class       = "db.t2.small"
-  db_subnet_group_name = aws_db_subnet_group.udacity_db_subnet_group.name
-}
-
-resource "aws_security_group" "db_sg_1" {
-  name   = "udacity-db-sg"
-  vpc_id =  var.vpc_id
+resource "aws_security_group" "rds_sg1" {
+  name   = "education_rds1"
+  vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port   = 3306
-    protocol    = "TCP"
-    to_port     = 3306
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 3306
-    protocol    = "TCP"
-    to_port     = 3306
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "education_rds1"
+  }
 }
+
+resource "aws_db_parameter_group" "education" {
+  name   = "education"
+  family = "postgres15"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
+
+resource "aws_db_instance" "udacity_instance" {
+  count                  =2
+  identifier             = "udacity-db-instance-${count.index}"
+  availability_zone     = ["us-east-2a", "us-east-2b"]
+  instance_class         = "db.m5.large"
+  allocated_storage      = 20
+  engine                 = "postgres"
+  engine_version         = "15.3"
+  username               = "edu"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.udacity_db_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg1.id]
+  parameter_group_name   = aws_db_parameter_group.education.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+  backup_retention_period    = 5
+}
+
