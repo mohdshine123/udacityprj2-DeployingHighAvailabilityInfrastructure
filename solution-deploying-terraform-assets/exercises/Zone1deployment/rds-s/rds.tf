0f728642c9b79f5773f27b25c574b21c28db2d8c
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2023-09-16 16:48:42 +0530

    Create rds.tf

---
+++ solution-deploying-terraform-assets/exercises/Zone1deployment/rds-s/rds.tf
@@ -0,0 +1,146 @@
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
+
+  name                 = "Udacity-rds-west"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_db_subnet_group" "udacity_db_subnet_group" {
+  name       = "udacity_db_subnet_group"
+  subnet_ids = module.vpc.public_subnets
+
+  tags = {
+    Name = "udacity_db_subnet_group"
+  }
+}
+
+resource "aws_security_group" "rds_sg1" {
+  name   = "education_rds1"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "education_rds1"
+  }
+}
+
+resource "aws_rds_cluster_parameter_group" "cluster_pg" {
+  name   = "udacity-pg-s"
+  #family = "aurora-postgresql15"
+  family ="aurora-mysql5.7"
+
+ # parameter {
+   # name  = "binlog_format"    
+   # value = "MIXED"
+   # apply_method = "pending-reboot"
+ # }
+
+  #parameter {
+   # name = "log_bin_trust_function_creators"
+   # value = 1
+   # apply_method = "pending-reboot"
+ # }
+}
+
+
+resource "aws_db_parameter_group" "education" {
+  name   = "education"
+ # family = "es15"
+  family ="aurora-mysql5.7"
+
+  #parameter {
+   # name  = "log_connections"
+   # value = "1"
+ # }
+}
+
+resource "aws_rds_cluster" "udacity_cluster-s" {
+  cluster_identifier       = "udacity-db-cluster-s"
+  #availability_zones       = ["us-west-1b", "us-west-1c"]
+  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_pg.name
+  database_name            = "udacityc2"
+  master_username          = "udacity"
+  master_password          = "MyUdacityPassword"
+  vpc_security_group_ids   = [aws_security_group.rds_sg1.id] 
+  db_subnet_group_name     = aws_db_subnet_group.udacity_db_subnet_group.name
+  #vpc_security_group_ids = var.vpc_security_group_ids
+  #db_subnet_group_name   = var.db_subnet_group_name
+  #db_cluster_parameter_group_name   = var.db_cluster_parameter_group_name
+  engine_mode              = "provisioned"
+  engine                 = "aurora-mysql"
+  engine_version         = "5.7.mysql_aurora.2.07.9"  
+#engine_version           = "5.6.mysql_aurora.1.19.1" 
+  #engine                 = "aurora-postgresql"
+  #engine_version         = "15.3"
+  #allocated_storage      = 20
+  skip_final_snapshot      = true
+  storage_encrypted        = false
+  backup_retention_period  = 5
+  depends_on = [aws_rds_cluster_parameter_group.cluster_pg]
+}
+
+
+#output "db_cluster_arn" {
+  #description = "The ARN of the RDS Cluster"
+  #value = aws_rds_cluster.udacity_cluster.arn
+#}
+
+
+resource "aws_rds_cluster_instance" "udacity_instance" {
+  count                = 2
+  identifier           = "udacity-db-instance-${count.index}"
+  cluster_identifier   = aws_rds_cluster.udacity_cluster.id
+  engine                 = "aurora-mysql"
+  instance_class       = "db.t3.medium"
+  #allocated_storage      = 20
+  availability_zone      =data.aws_availability_zones.available.names[count.index]
+  db_subnet_group_name = aws_db_subnet_group.udacity_db_subnet_group.name
+}
+
+#output "db_instance_arn" {
+ # description = "The ARN of the RDS instance"
+ # value       = aws_rds_cluster_instance.udacity_instance.arn[count.index]
+#}
+
+#resource "aws_db_instance" "udacity_instance" {
+ # count                  =2
+ # identifier             = "udacity-db-instance-${count.index}"
+  #availability_zone      =data.aws_availability_zones.available.names[0]
+  #availability_zone      =data.aws_availability_zones.available.names[count.index]
+  #azs                    = data.aws_availability_zones.available.names
+  #availability_zones      = module.vpc.azs
+  #availability_zone     = ["us-west-1b", "us-west-1c"]
+  #instance_class         = "db.m5.large"
+  #allocated_storage      = 20
+  #engine                 = "postgres"
+  #engine_version         = "15.3"
+  #username               = "edu"
+  #password               = var.db_password
+  #db_subnet_group_name   = aws_db_subnet_group.udacity_db_subnet_group.name
+  #vpc_security_group_ids = [aws_security_group.rds_sg1.id]
+  #parameter_group_name   = aws_db_parameter_group.education.name
+  #publicly_accessible    = true
+  #skip_final_snapshot    = true
+  #backup_retention_period = 5
+#}
+
