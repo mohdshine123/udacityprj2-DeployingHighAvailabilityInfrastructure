b783538367509ee8e0ffc73a5196009a11f46576
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2023-09-16 12:26:57 +0530

    Update rds.tf

--- solution-deploying-terraform-assets/exercises/Zone1deployment/RDSDeployment/rds-p/rds.tf
+++ solution-deploying-terraform-assets/exercises/Zone1deployment/RDSDeployment/rds-p/rds.tf
@@ -115,6 +115,7 @@ resource "aws_rds_cluster_instance" "udacity_instance" {
   #allocated_storage      = 20
   availability_zone      =data.aws_availability_zones.available.names[count.index]
   db_subnet_group_name = aws_db_subnet_group.udacity_db_subnet_group.name
+  parameter_group_name= aws_db_parameter_group.db_pg.name
 }
 
 output "db_instance_arn" {
